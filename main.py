c87491a63ee240de4e4c875e60ba8a432ee010ee
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2020-10-22 21:40:45 -0700

    more CLI

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from hash_table import Hash
 from truck import Truck
 from file_reader import File
 import delivery_process
+from package import Package
 
 # driver code
 
@@ -53,14 +54,38 @@ distance_labels = d.get_labels()  # MIGHT NOT NEED THIS
 # TODO: make the algorithm keep track of time starting at 8:00
 # TODO: allow for user input via the command line
 # TODO: calculate my run time -- off first glance I think my algo runs in O(n^2)
+
 print('WELCOME TO THE WGUPS COMMAND LINE INTERFACE')
 
 delivery_process.deliver_packages(package_hash_table, distance_data)
 
 print('See the options below for more WGUPS features:')
+print(' - to insert a package into the table type add [a]')
 print(' - for package inquiry type inquiry [i]')
-print(' - fo add a package type add [a]')
+print(' - to see package details at a specific time type time [t]')
 print(' - to print details of all packages type all [ap]')
+print('To exit or quit the program please type quit [q]')
 
+user_input = input('Enter command here:')
 
-user_input = input('Enter Here:')
+if user_input == 'q' or user_input == 'quit': exit('Thank you for using WGUPS')
+
+quit = False
+while not quit:
+    if user_input == 'a' or user_input == 'add':
+        try:
+            package_id = int(input('Enter a package id:'))
+        except ValueError:
+            print('\nERROR: package id must be a integer\n')
+            continue
+        address = str(input('Street address:'))
+        city = str(input('City:'))
+        state = str(input('State:'))
+        zip_code = str(input('Zip code:'))
+        delivery_time = str(input('Delivery time:'))
+        weight = str(input('Weight:'))
+        status = str(input('Status:'))
+        notes = str(input('Notes:'))
+        package = Package(str(package_id),address,city,state,zip_code,delivery_time,weight,status,notes)
+        print('The following package has been created:')
+        print(package)
